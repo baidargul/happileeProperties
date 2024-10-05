21966922dc39fe899ef131ccdf928867605c982f
Baidar Gul <baidargul@outlook.com>
2024-10-05 13:56:22 +0500

    created buyer create endpoint for testing

--- serveractions/commands/partials/buyer.ts
+++ serveractions/commands/partials/buyer.ts
@@ -1,12 +1,36 @@
 import axios from "axios";
 
-const listAll = async () => {
+async function listAll() {
   await axios.get("").then(async (res: any) => {
     const response = await res.data;
     return response;
   });
-};
+}
+
+async function get(id: number) {
+  await axios.get("").then(async (res: any) => {
+    const response = await res.data;
+    return response;
+  });
+}
+
+async function findByName(name: string) {
+  await axios.get("").then(async (res: any) => {
+    const response = await res.data;
+    return response;
+  });
+}
+
+async function create(data: any) {
+  await axios.post("/api/users/buyer").then(async (res: any) => {
+    const response = await res.data;
+    return response;
+  });
+}
 
 export const buyer = {
   listAll,
+  get,
+  findByName,
+  create,
 };
