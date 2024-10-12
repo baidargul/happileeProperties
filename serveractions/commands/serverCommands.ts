21ea7a9e3511242e877aec8451d7a208c3c6eaa1
Baidar Gul <baidargul@outlook.com>
2024-10-12 17:56:18 +0500

    added buyer to server

--- serveractions/commands/serverCommands.ts
+++ serveractions/commands/serverCommands.ts
@@ -1,9 +1,11 @@
 import { agent } from "./partials/agent";
 import { builder } from "./partials/builder";
+import { buyer } from "./partials/buyer";
 import { user } from "./partials/user";
 
 export const serverActions = {
   user,
+  buyer,
   builder,
   agent,
 };
