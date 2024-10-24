242e0dccaeba9e4b5c7f24cf7419cd5f8baec7d7
Baidar Gul <baidargul@outlook.com>
2024-10-24 20:56:00 +0500

    bug fixed

--- serveractions/commands/partials/property.ts
+++ serveractions/commands/partials/property.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+const apiPath = `http://localhost:3000/api/property/structure`;
 async function GET_ALLOTMENT_STRUCTURE() {
-  const response = await axios.get("/api/property/structure");
-  return response;
+  const response = await axios.get(apiPath);
+  return response.data;
 }
 
 export const property = {
