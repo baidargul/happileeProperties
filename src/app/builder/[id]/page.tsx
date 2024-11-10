784b098b6e5119710112f4d406b362df9e3531b0
Baidar Gul <baidargul@outlook.com>
2024-11-10 22:20:58 +0500

    server deployed and built is ready

--- src/app/builder/[id]/page.tsx
+++ src/app/builder/[id]/page.tsx
@@ -1,8 +1,21 @@
-import ListingDetailsSix from '@/components/ListingDetails/listing-details-6'
-import React from 'react'
+import ListingDetailsSix from "@/components/ListingDetails/listing-details-6";
+import React from "react";
 
-export default function Builder() {
-  return (
-	  <ListingDetailsSix/>
-  )
+export async function generateStaticParams() {
+  return [
+    { id: "1" },
+    { id: "2" },
+    { id: "3" },
+    { id: "4" },
+    { id: "5" },
+    { id: "6" },
+    { id: "7" },
+    { id: "8" },
+    { id: "9" },
+    { id: "10" },
+  ];
+}
+
+export default async function Builder() {
+  return <ListingDetailsSix />;
 }
--- src/app/property/[id]/page.tsx
+++ src/app/property/[id]/page.tsx
@@ -1,9 +1,21 @@
+import ListingDetailsFour from "@/components/ListingDetails/listing-details-4";
+import React from "react";
 
-import ListingDetailsFour from '@/components/ListingDetails/listing-details-4'
-import React from 'react'
+export async function generateStaticParams() {
+  return [
+    { id: "1" },
+    { id: "2" },
+    { id: "3" },
+    { id: "4" },
+    { id: "5" },
+    { id: "6" },
+    { id: "7" },
+    { id: "8" },
+    { id: "9" },
+    { id: "10" },
+  ];
+}
 
-export default function page() {
-  return (
-	<ListingDetailsFour/>
-  )
+export default async function page() {
+  return <ListingDetailsFour />;
 }
