f7dbab392aa5268230765bc6566feed2d0d5f735
Baidar Gul <baidargul@outlook.com>
2024-12-03 11:12:09 +0500

    filter query

---
+++ src/app/api/operations/users/filter/route.ts
@@ -0,0 +1,63 @@
+import { NextRequest } from "next/server";
+import prisma from "../../../../../../serveractions/commands/prisma";
+
+export async function GET(req: NextRequest) {
+  const response = {
+    status: 500,
+    message: "Internal Server Error",
+    data: null as any,
+  };
+  // Retrieve query parameters
+  const name = req.nextUrl.searchParams.get("name");
+  const email = req.nextUrl.searchParams.get("email");
+  const phone = req.nextUrl.searchParams.get("phone");
+  const type = req.nextUrl.searchParams.get("type");
+  const status = req.nextUrl.searchParams.get("status");
+  const deleted = req.nextUrl.searchParams.get("deleted");
+
+  try {
+    const filters: any = {};
+
+    if (name) {
+      filters.name = {
+        contains: name,
+        mode: "insensitive",
+      };
+    }
+    if (email) {
+      filters.email = {
+        contains: email,
+        mode: "insensitive",
+      };
+    }
+    if (phone) {
+      filters.phone = {
+        contains: phone,
+      };
+    }
+    if (type) {
+      filters.type = type;
+    }
+    if (status) {
+      filters.status = status;
+    }
+    if (deleted) {
+      filters.deleted = deleted;
+    }
+
+    const users = await prisma.user.findMany({
+      where: filters,
+    });
+
+    response.status = 200;
+    response.message = "Success";
+    response.data = users;
+    return new Response(JSON.stringify(response));
+  } catch (error: any) {
+    console.log("[SERVER ERROR]: " + error.message);
+    response.status = 500;
+    response.message = error.message;
+    response.data = null;
+    return new Response(JSON.stringify(response));
+  }
+}
