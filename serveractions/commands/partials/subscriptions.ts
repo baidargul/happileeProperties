0832ed1d7542d4be89690bd596e05cb04c33bae4
Baidar Gul <baidargul@outlook.com>
2024-12-08 17:47:39 +0500

    subscription properties can now be added

--- serveractions/commands/partials/subscriptions.ts
+++ serveractions/commands/partials/subscriptions.ts
@@ -2,6 +2,26 @@ import { accountTypes } from "@prisma/client";
 import axios from "axios";
 
 const apiPath = "/api/users/subscriptions";
+const propertyApiPath = "/api/users/subscriptions/properties";
+
+const property = {
+  create: async (
+    name: string,
+    limit: number,
+    subscriptionName: string,
+    type: string
+  ) => {
+    const data = {
+      name: name,
+      limit: limit,
+      subscriptionName: subscriptionName,
+      type: type,
+    };
+
+    const response = await axios.post(propertyApiPath, data);
+    return response.data;
+  },
+};
 
 async function listAll() {
   const response = await axios.get(apiPath);
@@ -22,4 +42,5 @@ async function create(name: string, type: accountTypes, price: number) {
 export const subscription = {
   listAll,
   create,
+  property,
 };
