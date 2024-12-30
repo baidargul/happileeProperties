789a80d634aab8c3ed9999736f741b80f7f03b05
Baidar Gul <baidargul@outlook.com>
2024-12-30 13:44:07 +0500

    added leads

--- serveractions/commands/serverCommands.ts
+++ serveractions/commands/serverCommands.ts
@@ -1,11 +1,13 @@
 import { agent } from "./partials/agent";
 import { builder } from "./partials/builder";
 import { buyer } from "./partials/buyer";
+import { leads } from "./partials/leads";
 import { property } from "./partials/property";
 import { user } from "./partials/user";
 
 export const serverActions = {
   user,
+  leads,
   buyer,
   builder,
   agent,
