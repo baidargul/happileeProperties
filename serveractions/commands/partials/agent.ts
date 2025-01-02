f199c48bd580e8d3c945886d1193274024a7f917
Baidar Gul <baidargul@outlook.com>
2025-01-02 12:07:38 +0500

    agent can now be created using form data and it now collects rera_link

--- serveractions/commands/partials/agent.ts
+++ serveractions/commands/partials/agent.ts
@@ -2,25 +2,45 @@ import axios from "axios";
 
 const apiPath = `/api/users/agent`;
 
-async function create(
-  id: string,
-  rera: string,
-  experience: number,
-  description?: string
-) {
-  const data = {
-    id,
-    rera,
-    experience,
-    description,
-  };
+// FOR JSON
+// async function create(
+//   id: string,
+//   rera: string,
+//   experience: number,
+//   description?: string
+// ) {
+//   const data = {
+//     id,
+//     rera,
+//     experience,
+//     description,
+//   };
 
-  const Result = await axios.post(apiPath, data).then(async (res: any) => {
+//   const Result = await axios.post(apiPath, data).then(async (res: any) => {
+//     const response = await res.data;
+//     return response;
+//   });
+
+//   return Result;
+// }
+async function create(data: any) {
+  try {
+    const res = await axios.post(apiPath, data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
     const response = await res.data;
     return response;
-  });
-
-  return Result;
+  } catch (error: any) {
+    console.error("Error while creating agent:", error);
+    console.error("ERROR:", error.message);
+    return {
+      status: 500,
+      message: error.message,
+      data: null,
+    };
+  }
 }
 
 async function listAll() {
