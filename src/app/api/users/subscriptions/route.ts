d2f25377940ecdd792ac6f1c7eecc2a04aeb1a5b
rapper015 <ashishoraon2309@gmail.com>
2025-01-12 11:59:37 +0530

    .

--- src/app/api/users/subscriptions/route.ts
+++ src/app/api/users/subscriptions/route.ts
@@ -243,79 +243,6 @@ export async function PATCH(req: NextRequest) {
   }
 }
 
-<<<<<<< HEAD
-// export async function PATCH(req: NextRequest) {
-//   const response = {
-//     status: 500,
-//     message: "Internal Server Error",
-//     data: null as any,
-//   };
-
-//   try {
-//     const data = await req.json();
-
-//     if (!data.subscriptionName) {
-//       response.status = 400;
-//       response.message = "Subscription name is required.";
-//       response.data = null;
-//       return new Response(JSON.stringify(response));
-//     }
-
-//     if (!data.type) {
-//       response.status = 400;
-//       response.message = "Account type is required.";
-//       response.data = null;
-//       return new Response(JSON.stringify(response));
-//     }
-
-//     let isExist: any = await prisma.subscription.findFirst({
-//       where: {
-//         name: data.subscriptionName,
-//         accountType: data.type,
-//       },
-//     });
-
-//     if (!isExist) {
-//       response.status = 400;
-//       response.message = "Subscription not found.";
-//       response.data = null;
-//       return new Response(JSON.stringify(response));
-//     }
-
-//     await prisma.subscription.update({
-//       where: {
-//         id: isExist.id,
-//       },
-//       data: {
-//         isPopular: true,
-//       },
-//     });
-
-//     await prisma.subscription.updateMany({
-//       where: {
-//         name: {
-//           not: data.subscriptionName,
-//         },
-//         accountType: data.type,
-//       },
-//       data: {
-//         isPopular: false,
-//       },
-//     });
-
-//     response.status = 200;
-//     response.message = "Subscription updated successfully. Hello Ashish";
-//     response.data = null;
-//     return new Response(JSON.stringify(response));
-//   } catch (error: any) {
-//     console.log("[SERVER ERROR]: " + error.message);
-//     response.status = 500;
-//     response.message = error.message;
-//     response.data = null;
-//     return new Response(JSON.stringify(response));
-//   }
-// }
-=======
 // FOR POPULAR TAG
 export async function PUT(req: NextRequest) {
   const response = {
@@ -390,4 +317,3 @@ export async function PUT(req: NextRequest) {
     return new Response(JSON.stringify(response));
   }
 }
->>>>>>> 29ed7d07a4a8ccc5c0ea708209b3fd71e99cd30d
