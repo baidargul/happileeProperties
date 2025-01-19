4abd04a4724568870d5d3f2021cccac3a1180b3c
rapper015 <ashishoraon2309@gmail.com>
2025-01-19 22:27:39 +0530

    LAST COMMIT MESSAGE WILL BE BUYER IS DONE NOT BUILDER

--- src/app/listing_17/page.tsx
+++ src/app/listing_17/page.tsx
@@ -2,12 +2,13 @@ import ListingSeventeen from "@/components/inner-listing/listing-17";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
-   title: "Listing Seventeen Happilee  ",
+   title: "Listing Seventeen Happile  ",
 };
 const index = () => {
    return (
       <Wrapper>
          {/* <ListingSeventeen /> */}
+
       </Wrapper>
    )
 }
